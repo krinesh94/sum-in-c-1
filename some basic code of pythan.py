78b2f8244636eec2683b829ddde158057ffaca92
krinesh94 <krineshfulbariya77@gmail.com>
2025-02-17 20:37:43 +0530

    Add files via upload

---
+++ some basic code of pythan.py
@@ -0,0 +1,15 @@
+print("hello world")
+print("my name is krinesh  fulbariya")
+print("my age is 18 years old")
+print("i am from india diu")
+print("ilike toplay criket, and football")
+
+
+
+name = "krinesh"
+age = 18
+city = "diu"
+
+print("my name is :" ,name)
+print("my age id :", age)
+print("i am living in the :", city)
